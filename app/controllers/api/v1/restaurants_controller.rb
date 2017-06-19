7918af94c44e8b1829822fc4fd0b3c42e3c1d9c2
vikram Panmand <vikky.panmand@gmail.com>
2017-06-19 19:08:42 +0530

    [update] added responder gem . and trying to make custom json response without jbuilder.

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
+  respond_to :json
   def index
     @restaurants = Restaurant.all_restaurants
+    respond_to do |format|
+      format.json { render json: @restaurants.to_json(only: [:name ,:review_count ,:latitude,:longitude, :city, :state, :zip, :avg_rating ])}
+    end
   end
-
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,4 +6,18 @@ class Restaurant < ApplicationRecord
     return data.sort_by!{|key| key['review_count']}
   end
 
+  def as_json(options={})
+    {:restaurant_name => "name",
+      :methods => [:address],
+      :rating => 'avg_rating',
+      :count => 'review_count'}
+  end
+
+  def address
+    {:latitude => 'latitude',
+        :longitude => 'longitude',
+        :city => 'city',
+        :state => 'state',
+        :zipcode => 'zip'}
+  end
 end
